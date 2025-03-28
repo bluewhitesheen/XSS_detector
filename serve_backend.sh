ef60897b1d335b6145954c566a412bf70f746f2e
bluewhite <terry25031069@gmail.com>
2025-03-28 15:34:03 +0800

    fix toml and add test.py to check html script syntex automatically

--- serve_backend.sh
+++ serve_backend.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
 poetry run uvicorn app.main:app --reload --host 127.0.0.1 --port 5555 # --root-path /myverysecretAPIs
+
+
---
+++ src/test.py
@@ -0,0 +1,15 @@
+import subprocess
+from oracle_tools import is_same_dom, do_xss_post_request
+import time
+
+endpoint = 'http://127.0.0.1:5555/vuln_backend/1.0/endpoint/'
+
+f = open("./XSS_dataset.csv", "r")
+lines = f.readlines()
+
+for line in lines:
+    payload, answer = line[:-2], line[-1:]
+    result_1 = do_xss_post_request(endpoint, 'abc')
+    result_2 = do_xss_post_request(endpoint, payload)
+    print(payload[:10], is_same_dom(result_1, result_2))
+    time.sleep(1)
